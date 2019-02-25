b1f9e4e37da39825410ff812e43c7d9b01e68523
patryk <patryk.bartosevic@gmail.com>
2019-02-25 19:35:33 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,5 @@
+module.exports = {
+  APPLICATION_ID: process.env.APPLICATION_ID || "PASTE YOUR ID",
+  SECRET: process.env.SECRET || "PASTE YOUR SECRET",
+  CALLBACK_URL: process.env.CALLBACK_URL || "http://localhost:3000"
+};
